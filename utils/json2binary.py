baa9c783436dcceb379c21d0a87e0abe55faea35
Lam Thai Nguyen <thainguyen2893@gmail.com>
2024-04-27 18:01:29 +0700

    remove unnecessary function arguments

--- utils/json2binary.py
+++ utils/json2binary.py
@@ -1,30 +1,26 @@
-import os
 import cv2
 import numpy as np
 import json
 
 
-def json2binary(json_path, original_512x512_path, save_path="dataset/annotated/annotated-T/") -> None:
+def json2binary(json_path: str) -> None:
     """
     Turns .json file from labelme to a binary image (512, 512)
 
     Args:
-        json_path: dataset/annotated/2_2_1_1_3_DSC09839.json
-        original_512x512_path: dataset/original_512x512/2_2_1_1_3_DSC09839.JPG
+        json_path: dataset/annotated/annotated-T/O. glaberrima/2_2_1_1_3_DSC09839.json
     """
     # Load JSON annotation file
+    file_name = json_path.split('/')[-1][:-5]
     with open(json_path) as f:
         data = json.load(f)
-
-    # Load image
-    image = cv2.imread(original_512x512_path)
-
+        
     # Create a black background image
-    background = np.zeros_like(image)
+    background = np.zeros((512, 512, 3))
 
     # Loop through objects in annotation
     for shape in data['shapes']:
-        label = shape['label']
+        _ = shape['label']
         points = shape['points']
         pts = np.array(points, dtype=np.int32)
         pts = pts.reshape((-1, 1, 2))
@@ -32,23 +28,12 @@ def json2binary(json_path, original_512x512_path, save_path="dataset/annotated/a
         # Fill object with white color
         cv2.fillPoly(background, [pts], (255, 255, 255))
 
-    index = len("dataset/original_512x512/")
-    cv2.imwrite(f'{save_path}{original_512x512_path[index:]}', background)
+    save_path = '/'.join(json_path.split('/')[:-1])
+    cv2.imwrite(f'{save_path}/{file_name}.jpg', background)
     
     
 if __name__ == "__main__":
     # ======Operating on single file======
-    json_path = "dataset/annotated/annotated-T/63_2_1_1_3_DSC01618.json"
-    index = len("dataset/annotated/annotated-T/")
-    original_512x512_path = f"dataset/original_512x512/{json_path[index:-5]}.jpg"
-    json2binary(json_path, original_512x512_path)
-    
-    # ======Automatic for the whole dataset======
-    # json_folder = "dataset/annotated/annotated-T"
-    # original_512x512_folder = "dataset/original_512x512"
-    # file_names = os.listdir(original_512x512_folder)
-    
-    # for img in file_names:
-    #     img_name = img[:-4]
-    #     json2binary(json_path=f"{json_folder}/{img_name}.json", original_512x512_path=f"{original_512x512_folder}/{img}")
+    json_path = "dataset/annotated/annotated-T/O. glaberrima/2_2_1_1_3_DSC09839.json"
+    json2binary(json_path)
     
