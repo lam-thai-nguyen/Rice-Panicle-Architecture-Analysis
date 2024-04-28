ec578a8e799df0dbee323b60ae2a98eacd2a0e53
Lam Thai Nguyen <thainguyen2893@gmail.com>
2024-04-28 15:02:21 +0700

    add description

--- utils/thin.py
+++ utils/thin.py
@@ -2,22 +2,28 @@ import cv2
 import numpy as np
 from skimage.morphology import skeletonize
 
+
 def thin(binary_path: str, method: str) -> None:
     """
     method takes 'zhang' or 'gradient'
+
+    ## Returns:
+
+    the skeleton as np.array
     """
     img = cv2.imread(binary_path, cv2.IMREAD_GRAYSCALE)
-    if method == 'zhang':
+    if method == "zhang":
         _, img_threshold = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
-        skeleton = skeletonize(img_threshold, method='zhang').astype(np.uint8) * 255
-    elif method == 'gradient':
+        skeleton = skeletonize(img_threshold, method="zhang").astype(np.uint8) * 255
+    elif method == "gradient":
         ...
     cv2.imshow("RESULT", skeleton)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     save_path = ""
-        
-    
+
+    return skeleton
+
+
 if __name__ == "__main__":
-    thin("dataset/annotated/annotated-K/O. glaberrima/2_2_1_1_3_DSC09839.jpg", 'zhang')
-    
+    thin("dataset/annotated/annotated-K/O. glaberrima/2_2_1_1_3_DSC09839.jpg", "zhang")
