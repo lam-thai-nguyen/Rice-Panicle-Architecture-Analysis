d56ac4a0965895d7324d23eb6ceefe79867c2c4b
Lam Thai Nguyen <thainguyen2893@gmail.com>
2024-04-28 21:04:39 +0700

    create separate functions

--- utils/thin.py
+++ utils/thin.py
@@ -3,26 +3,41 @@ import numpy as np
 from skimage.morphology import skeletonize
 
 
-def thin(binary_path: str, method: str) -> np.array:
+def thin(binary_path: str, method: str) -> np.ndarray:
     """
+    ## Arguments:
     method takes 'zhang' or 'gradient'
+    
+    ## Example 
+    binary_path = 'dataset/annotated/annotated-T/O. glaberrima/2_2_1_1_3_DSC09839.jpg'
 
     ## Returns:
 
     the skeleton as np.ndarray - shape = (512, 512)
     """
-    img = cv2.imread(binary_path, cv2.IMREAD_GRAYSCALE)
     if method == "zhang":
-        _, img_threshold = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
-        skeleton = skeletonize(img_threshold, method="zhang").astype(np.uint8) * 255
+        skeleton = _zhang_suen(binary_path)
     elif method == "gradient":
-        ...
-    cv2.imshow("RESULT", skeleton)
+        skeleton = _gradient_based_optimization(binary_path)
+        
+    cv2.imshow(method, skeleton)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
     return skeleton
 
+
+def _zhang_suen(binary_path: str) -> np.ndarray:
+    img = cv2.imread(binary_path, cv2.IMREAD_GRAYSCALE)
+    _, img_threshold = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+    skeleton = skeletonize(img_threshold, method="zhang").astype(np.uint8) * 255
+    return skeleton
+    
+
+def _gradient_based_optimization(binary_path: str) -> np.ndarray:
+    img = cv2.imread(binary_path, cv2.IMREAD_GRAYSCALE)
+    _, img_threshold = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+    ...
 
 if __name__ == "__main__":
     thin("dataset/annotated/annotated-K/O. glaberrima/2_2_1_1_3_DSC09839.jpg", "zhang")
