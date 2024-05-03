120a93247b38c19ddc32957071a8f2b25b7fca59
Lam Thai Nguyen <thainguyen2893@gmail.com>
2024-05-03 16:14:47 +0700

    plotting tool for thin.py

---
+++ utils/thin_plotting.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_preprocess(raw_bin_img: np.ndarray, processed_bin_img: np.ndarray) -> None:
+    _, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(15, 7))
+    ax1.imshow(raw_bin_img, cmap='gray')
+    ax1.set_title("Raw")
+    ax1.axis('off')
+    
+    ax2.imshow(processed_bin_img, cmap='gray')
+    ax2.set_title("Processed")
+    ax2.axis('off')
+    
+    plt.suptitle("PREPROCESSING")
+    plt.show()
+    
+
+def plot_skeleton(raw_skeleton: np.ndarray, processed_skeleton: np.ndarray) -> None:
+    _, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(15, 7))
+    ax1.imshow(raw_skeleton, cmap='gray')
+    ax1.set_title("Raw")
+    ax1.axis('off')
+    
+    ax2.imshow(processed_skeleton, cmap='gray')
+    ax2.set_title("Processed")
+    ax2.axis('off')
+    
+    plt.suptitle("PREPROCESSING")
+    plt.show()
+    
+
+def plot_thin(raw_bin_img: np.ndarray, processed_bin_img, raw_skeleton: np.ndarray, processed_skeleton: np.ndarray) -> None:
+    _, axes = plt.subplots(nrows=2, ncols=2, figsize=(8, 8))
+    (ax1, ax2, ax3, ax4) = axes.flatten()
+    
+    ax1.imshow(raw_bin_img, cmap='gray')
+    ax1.set_title("Raw binary")
+    ax1.axis('off')
+    
+    ax2.imshow(processed_bin_img, cmap='gray')
+    ax2.set_title("Processed binary")
+    ax2.axis('off')
+    
+    ax3.imshow(raw_skeleton, cmap='gray')
+    ax3.set_title("Raw skeleton")
+    ax3.axis('off')
+
+    ax4.imshow(processed_skeleton, cmap='gray')
+    ax4.set_title("Processed skeleton")
+    ax4.axis('off')
+    
+    plt.suptitle("THINNING")
+    plt.show()
