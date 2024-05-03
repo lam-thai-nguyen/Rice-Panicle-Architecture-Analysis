b9801df8162bfb5efbeefd7c8ac77b38782f02f3
Lam Thai Nguyen <thainguyen2893@gmail.com>
2024-05-03 15:47:30 +0700

    add _plot_thin func

--- utils/thin.py
+++ utils/thin.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from skimage.morphology import skeletonize, binary_erosion
 
 
-def thin(binary_path: str, method: str) -> np.ndarray:
+def thin(binary_path: str, method: str, plot_result=False) -> np.ndarray:
     """
     ## Arguments:
     method takes 'zhang' or 'gradient'
@@ -16,60 +16,86 @@ def thin(binary_path: str, method: str) -> np.ndarray:
 
     the skeleton as np.ndarray - shape = (512, 512)
     """
+    raw_bin_img = cv2.imread(binary_path, cv2.IMREAD_GRAYSCALE)
+    
     if method == "zhang":
-        skeleton = _zhang_suen(binary_path)
+        raw_skeleton, processed_skeleton, processed_bin_img = _zhang_suen(raw_bin_img)
     elif method == "gradient":
-        skeleton = _gradient_based_optimization(binary_path)
+        raw_skeleton, processed_skeleton, processed_bin_img = _gradient_based_optimization(raw_bin_img)
 
-    cv2.imshow(method, skeleton)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # cv2.imshow(method, processed_skeleton)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    
+    if plot_result:
+        _plot_thin(raw_bin_img, processed_bin_img, raw_skeleton, processed_skeleton)
 
-    return skeleton
-
-
-def _zhang_suen(binary_path: str) -> np.ndarray:
-    img = cv2.imread(binary_path, cv2.IMREAD_GRAYSCALE)
-    processed = _preprocess(img, 1)
-    skeleton = skeletonize(processed, method="zhang").astype(np.uint8) * 255
-    return skeleton
+    return processed_skeleton
 
 
-def _gradient_based_optimization(binary_path: str) -> np.ndarray:
-    img = cv2.imread(binary_path, cv2.IMREAD_GRAYSCALE)
-    processed = _preprocess(img)
+def _zhang_suen(bin_img: np.ndarray) -> list[np.ndarray]:
+    processed_bin_img = _preprocess(bin_img)
+    raw_skeleton = skeletonize(bin_img, method="zhang").astype(np.uint8) * 255
+    processed_skeleton = skeletonize(processed_bin_img, method="zhang").astype(np.uint8) * 255
+    return raw_skeleton, processed_skeleton, processed_bin_img
+
+
+def _gradient_based_optimization(bin_img: np.ndarray) -> np.ndarray:
     ...
 
-def _preprocess(img: np.ndarray, plot_result=False) -> np.ndarray:
+def _preprocess(bin_img: np.ndarray, plot_result=False) -> np.ndarray:
     """
     threshold, erosion, dilation, opening, closing to remove noise
     """
-    _, img_threshold = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
-    processed = img_threshold
+    _, img_threshold = cv2.threshold(bin_img, 127, 255, cv2.THRESH_BINARY)
+    processed_bin_img = img_threshold
     
     if plot_result:
-        _plot_preprocess(img, processed)
+        _plot_preprocess(bin_img, processed_bin_img)
         
-    return processed
+    return processed_bin_img
 
 
-def _plot_preprocess(pre_img: np.ndarray, post_img: np.ndarray) -> None:
-    _, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(10, 5))
-    ax1.imshow(pre_img, cmap='gray')
-    ax1.set_title("Original")
+def _plot_preprocess(raw_bin_img: np.ndarray, processed_bin_img: np.ndarray) -> None:
+    _, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(8, 4))
+    ax1.imshow(raw_bin_img, cmap='gray')
+    ax1.set_title("Raw")
     ax1.axis('off')
     
-    ax2.imshow(post_img, cmap='gray')
+    ax2.imshow(processed_bin_img, cmap='gray')
     ax2.set_title("Processed")
     ax2.axis('off')
     
     plt.suptitle("PREPROCESSING")
     plt.show()
-    return None
+
+
+def _plot_thin(raw_bin_img: np.ndarray, processed_bin_img, raw_skeleton: np.ndarray, processed_skeleton: np.ndarray) -> None:
+    _, axes = plt.subplots(nrows=2, ncols=2, figsize=(8, 8))
+    (ax1, ax2, ax3, ax4) = axes.flatten()
     
+    ax1.imshow(raw_bin_img, cmap='gray')
+    ax1.set_title("Raw binary")
+    ax1.axis('off')
+    
+    ax2.imshow(processed_bin_img, cmap='gray')
+    ax2.set_title("Processed binary")
+    ax2.axis('off')
+    
+    ax3.imshow(raw_skeleton, cmap='gray')
+    ax3.set_title("Raw skeleton")
+    ax3.axis('off')
+
+    ax4.imshow(processed_skeleton, cmap='gray')
+    ax4.set_title("Processed skeleton")
+    ax4.axis('off')
+    
+    plt.suptitle("THINNING")
+    plt.show()
+
 
 if __name__ == "__main__":
-    thin("dataset/annotated/annotated-K/O. glaberrima/64_2_1_3_2_DSC01622.jpg", "zhang")
+    thin("dataset/annotated/annotated-K/O. glaberrima/64_2_1_3_2_DSC01622.jpg", "zhang", True)
 
     # Erosion needed examples
     # _zhang_suen("dataset/annotated/annotated-K/O. glaberrima/56_2_1_2_3_DSC01608.jpg")
