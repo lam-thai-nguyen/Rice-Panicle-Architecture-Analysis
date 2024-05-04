12028ca26d52d78df434807691f833483ba73830
Lam Thai Nguyen <thainguyen2893@gmail.com>
2024-05-05 01:25:08 +0700

    add info argument for 2 functions

--- utils/thin_plotting.py
+++ utils/thin_plotting.py
@@ -1,7 +1,9 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_preprocess(raw_bin_img: np.ndarray, processed_bin_img: np.ndarray) -> None:
+def plot_preprocess(raw_bin_img: np.ndarray, processed_bin_img: np.ndarray, info: list[str]) -> None:
+    file_name, model = info
     _, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(16, 8))
     ax1.imshow(raw_bin_img, cmap='gray')
     ax1.set_title("Raw")
@@ -12,10 +14,18 @@ def plot_preprocess(raw_bin_img: np.ndarray, processed_bin_img: np.ndarray) -> N
     ax2.axis('off')
     
     plt.suptitle("PREPROCESSING")
+    # Specify save path
+    directory = f"results/preprocess/{model}/{file_name[:-4]}"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # Save the plot
+    plt.savefig(os.path.join(directory, f"binary_{file_name}"))
     plt.show()
     
 
-def plot_skeleton(raw_skeleton: np.ndarray, processed_skeleton: np.ndarray) -> None:
+def plot_skeleton(raw_skeleton: np.ndarray, processed_skeleton: np.ndarray, info: list[str]) -> None:
+    file_name, model = info
     _, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(16, 8))
     ax1.imshow(raw_skeleton, cmap='gray')
     ax1.set_title("Raw")
@@ -26,6 +36,13 @@ def plot_skeleton(raw_skeleton: np.ndarray, processed_skeleton: np.ndarray) -> N
     ax2.axis('off')
     
     plt.suptitle("PREPROCESSING")
+    # Specify save path
+    directory = f"results/preprocess/{model}/{file_name[:-4]}"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # Save the plot
+    plt.savefig(os.path.join(directory, f"skeleton_{file_name}"))
     plt.show()
     
 
@@ -51,3 +68,17 @@ def plot_thin(raw_bin_img: np.ndarray, processed_bin_img, raw_skeleton: np.ndarr
     
     plt.suptitle("THINNING")
     plt.show()
+    
+    
+def extract_info(binary_path: str) -> list[str]:
+    info = binary_path.split('/')
+    file_name = info[-1]
+    model = "annotated"
+    model_list = ['ACS', 'DEEPCRACK', 'RUC_NET', 'SEGNET', 'U2CRACKNEL', 'UNET']
+    for i in info:
+        if i in model_list:
+            model = i
+            break
+    
+    return file_name, model
+    
