076838b6af8c19786d253cee6c73368ab8800d54
Lam Thai Nguyen <thainguyen2893@gmail.com>
2024-05-07 15:13:39 +0700

    update util

--- src/utils/json2binary.py
+++ src/utils/json2binary.py
@@ -5,35 +5,40 @@ import json
 
 def json2binary(json_path: str) -> None:
     """
-    Turns .json file from labelme to a binary image (512, 512)
+    ## Description
+    Turning a .json file from labelme to a binary image shape=(512, 512).
 
-    Args:
-        json_path: dataset/annotated/annotated-T/O. glaberrima/2_2_1_1_3_DSC09839.json
+    ## Argument
+    json_path (str)
+
+    ## Example
+    json_path = images/annotated/annotated-T/O. glaberrima/1_2_1_1_1_DSC01251.json
+
+    ## Returns
+    None
     """
-    # Load JSON annotation file
-    file_name = json_path.split('/')[-1][:-5]
+    # Extract information
+    info = json_path.split("/")
+    file_name = info[-1][:-5]  # 1_2_1_1_1_DSC01251
+    species = info[-2]
+    user = info[-3]
+
+    # Load annotation file
     with open(json_path) as f:
         data = json.load(f)
-        
-    # Create a black background image
-    background = np.zeros((512, 512, 3))
 
-    # Loop through objects in annotation
-    for shape in data['shapes']:
-        _ = shape['label']
-        points = shape['points']
+    # Create a black background image
+    binary_image = np.zeros((512, 512, 3))
+
+    # Loop through objects in json file
+    for shape in data["shapes"]:
+        _ = shape["label"]
+        points = shape["points"]
         pts = np.array(points, dtype=np.int32)
         pts = pts.reshape((-1, 1, 2))
-        
-        # Fill object with white color
-        cv2.fillPoly(background, [pts], (255, 255, 255))
 
-    save_path = '/'.join(json_path.split('/')[:-1])
-    cv2.imwrite(f'{save_path}/{file_name}.jpg', background)
-    
-    
-if __name__ == "__main__":
-    # ======Operating on single file======
-    json_path = "dataset/annotated/annotated-T/O. glaberrima/2_2_1_1_3_DSC09839.json"
-    json2binary(json_path)
-    
+        # Fill object with white color
+        cv2.fillPoly(binary_image, [pts], (255, 255, 255))
+
+    save_path = f"images/annotated/{user}/{species}/{file_name}.jpg"
+    cv2.imwrite(save_path, binary_image)
