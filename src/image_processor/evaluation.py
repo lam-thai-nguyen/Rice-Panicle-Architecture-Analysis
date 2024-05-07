30b44584c6d2e65260cc167b3be74e3cdc97c126
Lam Thai Nguyen <thainguyen2893@gmail.com>
2024-05-07 13:31:36 +0700

    add new file

--- src/image_processor/evaluation.py
+++ src/image_processor/evaluation.py
@@ -1,1 +1,18 @@
 import numpy as np
+from sklearn.metrics import f1_score
+
+
+def _f1_score(y_true: np.ndarray, y_pred: np.ndarray, _return_metrics: bool = False) -> float:
+    """
+    ## Description
+    Compute the f1-score as the accuracy of rice panicle junction detection.
+
+    ## Arguments
+    - y_true (np.ndarray)
+    - y_pred (np.ndarray)
+    - _return_metrics (bool) = False -> if True, returns precision and recall
+
+    ## Returns:
+    f1, (pr, rc) -> f1-score, (precision, recall)
+    """
+    
