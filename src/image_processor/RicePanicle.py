376eb9bafc53fa1f8de2de3537a2f955e2d657b5
Lam Thai Nguyen <thainguyen2893@gmail.com>
2024-05-27 09:59:10 +0700

    update

--- src/image_processor/RicePanicle.py
+++ src/image_processor/RicePanicle.py
@@ -67,15 +67,17 @@ class RicePanicle:
             return _f1_score(y_true, y_pred, _return_metrics)
         
     class DetectionAccuracy:
-        def __init__(self, file_path: str, all_junctions: list[float], main_axis: list[float], high_order: list[float]):
-            info = file_path.split('/')
-            self.name = info[-1][:-4]
+        def __init__(self, name: str, all_junctions: list[float], main_axis: list[float], high_order: list[float], model: str = None):
+            self.name = name
             self.f1_1, self.pr_1, self.rc_1 = all_junctions
             self.f1_2, self.pr_2, self.rc_2 = main_axis
             self.f1_3, self.pr_3, self.rc_3 = high_order
+            self.model = model
             
         def show(self):
             print(f"==>> {self.name} <<==")
             print(f"All junctions: f1-score: {self.f1_1:.4f}, precision: {self.pr_1:.4f}, recall: {self.rc_1:.4f}")
             print(f"Main axis junctions: f1-score: {self.f1_2:.4f}, precision: {self.pr_2:.4f}, recall: {self.rc_2:.4f}")
             print(f"High order junctions: f1-score: {self.f1_3:.4f}, precision: {self.pr_3:.4f}, recall: {self.rc_3:.4f}")
+            if self.model is not None:
+                print(f"Detected by Model: {self.model}")
